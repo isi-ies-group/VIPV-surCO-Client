6612da062f77ba76f53d252cde406fe408212103
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2025-02-14 12:23:33 +0000

    Update ExtensionFunctions.kt

--- app/src/main/java/com/example/beaconble/ExtensionFunctions.kt
+++ app/src/main/java/com/example/beaconble/ExtensionFunctions.kt
@@ -2,6 +2,9 @@ package com.example.beaconble
 
 import androidx.lifecycle.MutableLiveData
 
+/**
+ * Notifies the observers of a MutableLiveData.
+ */
 fun <T> MutableLiveData<T>.notifyObservers() {
     this.value = this.value
 }
