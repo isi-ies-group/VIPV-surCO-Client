014df2688938077de9fa65da765f5a4fd96f8e5b
Echedey Luis Álvarez <80125792+echedey-ls@users.noreply.github.com>
2025-05-22 13:17:38 +0100

    clarify deprecation fix for the future

--- app/src/main/java/es/upm/ies/surco/session_logging/SessionFilesObserver.kt
+++ app/src/main/java/es/upm/ies/surco/session_logging/SessionFilesObserver.kt
@@ -6,8 +6,9 @@ import java.io.File
 class SessionFilesObserver(
     private val directory: File, private val onFileChanged: (List<File>) -> Unit
 ) {
-    private val fileObserver = object :
-        FileObserver(directory.absolutePath, CREATE or DELETE) {  // update when minSdk >= 29
+    private val fileObserver = object : FileObserver(
+        directory.absolutePath, CREATE or DELETE
+    ) {  // update "directory.absolutePath" > "directory" when minSdk >= 29
         override fun onEvent(event: Int, path: String?) {
             if (path != null) {
                 // Update the session files list when a file is created or deleted
